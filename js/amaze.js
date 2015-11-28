6b463d5b69bd04b9566fec18a30a0528bb698317
Kevin Kreiser <kevinkreiser@gmail.com>
2015-11-28 18:02:08 -0500

    marginally better start and goal placement

--- js/amaze.js
+++ js/amaze.js
@@ -134,10 +134,15 @@ var reset = function () {
     }
   }
 
-  //TODO: pick start and end based on path length
-  
-  start = [round(Math.random() * (dimensions - 1)), round(Math.random() * (dimensions - 1))];
-  end = [round(Math.random() * (dimensions - 1)), round(Math.random() * (dimensions - 1))];
+  //randomly start along the perimeter of the maze and end on the opposite side
+  if(round(Math.random())) {
+    start = [round(Math.random()) * (dimensions - 1), round(Math.random() * (dimensions - 1))];
+    end = [start[0] ? 0 : dimensions - 1, round(Math.random()) * (dimensions - 1)];
+  }
+  else {
+    start = [round(Math.random() * (dimensions - 1)), round(Math.random()) * (dimensions - 1)];
+    end = [round(Math.random()) * (dimensions - 1), start[1] ? 0 : dimensions - 1];
+  }
   path = [];
 
   //(function depth_first_backtrack(x, y){
